0139c4568b0ba4a9f71d4486eba7d849b6397789
delfinarawson <drawson@udesa.edu.ar>
2023-03-24 16:17:40 -0300

    cree el repositorio

---
+++ practica3/controllers/heroesController.js
@@ -0,0 +1,18 @@
+let db = require('../db/index')
+
+let heroesController = {
+    index: function(req, res){
+        return res.render('index', { heroes: db.lista})
+    },
+
+    show: function(req, res){
+        return res.send('Detalle de un heroe.')
+    },
+
+    detail: function(req, res){
+        return res.send('Bio del heroe')
+    },
+
+}
+
+module.exports = heroesController;
